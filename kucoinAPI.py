e073089596e755132c78775fb8340da1288db876
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2023-05-27 18:33:53 -0300

    Add files via upload

--- kucoinAPI.py
+++ kucoinAPI.py
@@ -2,28 +2,39 @@ import kucoin.client as kc
 import json
 
 
+def login():
 
-API_KEY = '64710815ba02b40001f9cc3d'
-API_SECRET = 'b03e7827-4c1f-4064-84d7-c8c6758c38ad'
+    global client
+    API_KEY = 'public key'                      #        test'64710815ba02b40001f9cc3d'
+    API_SECRET = 'secret key'       #        test 'b03e7827-4c1f-4064-84d7-c8c6758c38ad'
 
-client = kc.Client(API_KEY, API_SECRET, 'shiro123', sandbox=True)
+    client = kc.Client(API_KEY, API_SECRET, 'passphrase')
 
 
+def main():
 
-buy_quantity = 100.0
-pair = input('par: ') #'BTC'
-price = float(client.get_ticker(f'{pair}-USDT').get('price'))
+    login()
 
-buy_order = client.create_market_order(f'{pair}-USDT', 'buy', funds=buy_quantity)
+    buy_quantity = 9.61 ## value in USDT to use
 
-accounts = client.get_accounts()
+    pair = input('currency: ') # any
+    price = float(client.get_ticker(f'{pair}-USDT').get('price'))
 
-for item in accounts:
-    if item.get('currency') == pair and item.get('type') == 'trade':
-        sell_quantity = item.get('available')
+    buy_order = client.create_market_order(f'{pair}-USDT', 'buy', funds=buy_quantity)
+    accounts = client.get_accounts()
+
+    for item in accounts:
+        if item.get('currency') == pair and item.get('type') == 'trade':
+            sell_quantity = int(float(item.get('available')) * 100000000)/100000000
+   
+    after_price = int(price * 1.5 * 100000000)/100000000   #truncating long float numbers to only 10^-8 precision
+                            # 1.5 is the price ratio to sell (+50%) 
+    sell_order = client.create_limit_order(f'{pair}-USDT', 'sell', after_price, sell_quantity)
+
+    json.dump(client.get_accounts(), open('account.json', 'w'), indent=4)
+
+if __name__ == '__main__':
+    main()
     
-after_price = price * 1.5
-sell_order = client.create_limit_order(f'{pair}-USDT', 'sell', after_price, sell_quantity)
 
-json.dump(client.get_accounts(), open('account.json', 'w'), indent=4)
 #client.get_ticker('BTC-USDT').get('price')
